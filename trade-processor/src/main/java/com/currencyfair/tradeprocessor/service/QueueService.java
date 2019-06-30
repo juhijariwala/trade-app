543e45ee05e9397a730e66a741b59ca68f1aa249
Juhi Jariwala <juhijari@thoughtworks.com>
2019-06-30 14:40:36 +0530

    Adding web socket

--- trade-processor/src/main/java/com/currencyfair/tradeprocessor/service/QueueService.java
+++ trade-processor/src/main/java/com/currencyfair/tradeprocessor/service/QueueService.java
@@ -2,6 +2,8 @@ package com.currencyfair.tradeprocessor.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,15 +11,17 @@ public class QueueService {
 
     private static final Logger log = LoggerFactory.getLogger(QueueService.class);
 
+    @Autowired
+    private SimpMessagingTemplate template;
+
     public void handle(String message) {
         log.info("Handling started for queue one message: {}", message);
+        template.convertAndSend("/topic/trades", message);
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        log.info("Handling stop");
     }
 
 }
